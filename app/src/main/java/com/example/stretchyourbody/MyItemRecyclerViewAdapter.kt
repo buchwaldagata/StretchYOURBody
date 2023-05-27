032c8c975c25e362447d54cd42d5de04f6ae5c47
TrocheTroszeczke <anna.czachorek@student.put.poznan.pl>
2023-05-27 18:30:55 +0200

    Drugi raz wrzucam to samo;)

---
+++ app/src/main/java/com/example/stretchyourbody/MyItemRecyclerViewAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.stretchyourbody
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.example.stretchyourbody.placeholder.PlaceholderContent.PlaceholderItem
+import com.example.stretchyourbody.databinding.FragmentTrainingListBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class MyItemRecyclerViewAdapter(
+    private val values: List<PlaceholderItem>
+) : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentTrainingListBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentTrainingListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.itemNumber
+        val contentView: TextView = binding.content
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}
