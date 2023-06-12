8f96cb4736e0fa68233fbe4bcac270b69dbbfe10
buchw <buchwaldagat@gmail.com>
2023-06-12 15:42:48 +0200

    progress

--- app/src/main/java/com/example/stretchyourbody/RegisterActivity.kt
+++ app/src/main/java/com/example/stretchyourbody/RegisterActivity.kt
@@ -4,16 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
 
         val loginText: TextView = findViewById(R.id.textView_login_now)
         loginText.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        //get email and password
+
     }
+
+
+
 }
