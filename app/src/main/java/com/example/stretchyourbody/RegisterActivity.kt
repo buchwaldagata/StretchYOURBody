d878fb0672eca4483720819a99d08b7e40b45c0e
buchw <buchwaldagat@gmail.com>
2023-06-12 20:06:56 +0200

    udana rejestracja

--- app/src/main/java/com/example/stretchyourbody/RegisterActivity.kt
+++ app/src/main/java/com/example/stretchyourbody/RegisterActivity.kt
@@ -3,7 +3,10 @@ package com.example.stretchyourbody
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -26,10 +29,52 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+        val registerButton: Button = findViewById(R.id.button_register)
+
+
+
         //get email and password
+        registerButton.setOnClickListener{
+            performSignUp()
+        }
+
 
     }
 
+    private fun performSignUp() {
+        val email = findViewById<EditText>(R.id.editText_email_register)
+        val password = findViewById<EditText>(R.id.editText_password_register)
+
+        if (email.text.isEmpty() || password.text.isEmpty()){
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        val inputEmail = email.text.toString()
+        val inputPassword = password.text.toString()
+
+        auth.createUserWithEmailAndPassword(inputEmail,inputPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, let move to the next activity i.e MainActivity
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(baseContext, "Sukces", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText( baseContext, "Authentication failed. ",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Error occurred ${it.localizedMessage}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
 
 
 }
