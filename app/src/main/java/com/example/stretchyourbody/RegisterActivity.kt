bc917011c72a8543a4560e821ac2ea46a434951e
buchw <buchwaldagat@gmail.com>
2023-06-12 14:32:28 +0200

    dodanie przejscia z rejestracji do zaloguj

--- app/src/main/java/com/example/stretchyourbody/RegisterActivity.kt
+++ app/src/main/java/com/example/stretchyourbody/RegisterActivity.kt
@@ -1,11 +1,19 @@
 package com.example.stretchyourbody
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val loginText: TextView = findViewById(R.id.textView_login_now)
+        loginText.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
