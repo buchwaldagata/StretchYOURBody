3ccb2f4d28dfd67ca6113400435ec5cef9374f54
buchw <buchwaldagat@gmail.com>
2023-06-12 11:27:27 +0200

    logowanie cz1 front

---
+++ app/src/main/java/com/example/stretchyourbody/RegistrationActivity.kt
@@ -0,0 +1,30 @@
+package com.example.stretchyourbody
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class RegistrationActivity : AppCompatActivity() {
+
+    private val fbAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        isCurrentUser()
+    }
+
+    private fun isCurrentUser() {
+        fbAuth.currentUser?.let{ auth ->
+            val intent = Intent(applicationContext, MainActivity::class.java).apply {
+                flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+            startActivity(intent)
+        }
+    }
+}
