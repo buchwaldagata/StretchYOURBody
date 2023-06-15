17e84d162b370e07367cf55251b73bca751875ed
TrocheTroszeczke <anna.czachorek@student.put.poznan.pl>
2023-06-15 22:17:52 +0200

    Dodawanie ćwiczenia

--- app/src/main/java/com/example/stretchyourbody/AddTrainingActivity.kt
+++ app/src/main/java/com/example/stretchyourbody/AddTrainingActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 
 class AddTrainingActivity : AppCompatActivity() {
@@ -24,9 +25,22 @@ class AddTrainingActivity : AppCompatActivity() {
 
         val buttonNext = findViewById<Button>(R.id.buttonNext)
         buttonNext.setOnClickListener {
-            openAddExercisesActivity()
+            if (validateFields()) {
+                openAddExercisesActivity()
+            } else {
+                showAlertDialog("Uzupełnij wszystkie pola!")
+            }
         }
     }
+
+    private fun validateFields(): Boolean {
+        val name = editTextName.text.toString().trim()
+        val description = editTextDescription.text.toString().trim()
+        val duration = editTextDuration.text.toString().trim()
+
+        return name.isNotEmpty() && description.isNotEmpty() && duration.isNotEmpty()
+    }
+
 
     private fun openAddExercisesActivity() {
         val name = editTextName.text.toString()
@@ -38,5 +52,15 @@ class AddTrainingActivity : AppCompatActivity() {
         intent.putExtra("description", description)
         intent.putExtra("duration", duration)
         startActivity(intent)
+    }
+
+    private fun showAlertDialog(message: String) {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setMessage(message)
+        alertDialogBuilder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 }
