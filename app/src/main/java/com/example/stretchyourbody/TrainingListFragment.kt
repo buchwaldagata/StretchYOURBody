be41a15a800d14f97ac29cf50c646bf65cdab4fb
buchw <buchwaldagat@gmail.com>
2023-06-15 22:25:40 +0200

    Dodanie listy treningow z bazy danych, zaslonieta pierwsza nazwa treningu

--- app/src/main/java/com/example/stretchyourbody/TrainingListFragment.kt
+++ app/src/main/java/com/example/stretchyourbody/TrainingListFragment.kt
@@ -10,33 +10,42 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.fragment.app.ListFragment
+import com.example.stretchyourbody.data.Trening
+import com.example.stretchyourbody.data.User
 
 class TrainingListFragment : ListFragment() {
-    val trainings: TrainingsList = TrainingsList()
-    var trainingNames = trainings.getAllTrainingsNames()
+    var user: User? = null
+    var trainings: List<Trening> = listOf()
+    var trainingNames: MutableList<String> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        trainingNames = trainings.getAllTrainingsNames()
+        user = arguments?.getSerializable("test") as User?
+        trainings = user?.treningi ?: listOf()
+
+        trainings.forEach {
+            trainingNames.add(it.nazwaTreningu.toString())
+        }
+
         val adapter = ArrayAdapter(
             inflater.context, R.layout.simple_list_item_1, trainingNames
         )
         listAdapter = adapter
 
-        val data = arguments?.getSerializable("test")
+
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 
-    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
-        Log.e("lista", "position " + position.toString())
-        Log.e("lista", trainingNames[position])
-
-        val trainingName = trainingNames[position]
-        val intent = Intent(activity, TrainingStartActivity::class.java)
-        intent.putExtra("trainingName", trainingName)
-        startActivity(intent)
-    }
+//    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+//        Log.e("lista", "position " + position.toString())
+//        Log.e("lista", trainingNames[position])
+//
+//        val trainingName = trainingNames[position]
+//        val intent = Intent(activity, TrainingStartActivity::class.java)
+//        intent.putExtra("trainingName", trainingName)
+//        startActivity(intent)
+//    }
 
 }
